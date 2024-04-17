bf139dd7cd9dba93551fe94011eaf5ca5347df19
Zalan Fulop <zalan12f@gmail.com>
2024-04-17 21:50:36 +0200

    waker-word

--- project.py
+++ project.py
@@ -2,18 +2,22 @@ import pyttsx3
 import speech_recognition as sr
 
 r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
-
-try:
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-
 engine = pyttsx3.init()
-engine.say("Hello Blanka, Zalan said that " + r.recognize_google(audio))
-engine.runAndWait()
+wake_word = "hey Bob"
+while True:
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+
+    try:
+        text = r.recognize_google(audio)
+        print(text)
+        if wake_word.casefold() in text.casefold():
+            print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+            engine.say("You said " + r.recognize_google(audio))
+            engine.runAndWait()
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
