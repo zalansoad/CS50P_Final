770d94ac9b96e3f8292ad5bac0d193a8e17a2177
Zalan Fulop <zalan12f@gmail.com>
2024-04-30 20:23:30 +0200

    regex fix

--- project.py
+++ project.py
@@ -7,24 +7,26 @@ import re
 def main():
     r = sr.Recognizer()
     engine = pyttsx3.init()  
-    voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
+    voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-GB_HAZEL_11.0"
     engine.setProperty('voice', voice_id)
-    wake_word = "hey bob"
+    name = "Lucy"
+    wake_word = f"hey {name}"
     stop_word = "stop the program"
-    welcome = "Welcome, I am Bob your personal assistant."
+    welcome = f"Welcome, I am {name} your personal assistant."
     text_to_speech(engine, welcome)
-    recognise(r, engine, wake_word, stop_word)
+    recognise(r, engine, wake_word, stop_word, name.lower())
     
-def recognise(r, engine, wake, stop):
+def recognise(r, engine, wake, stop, name):
     with sr.Microphone() as source:
         while True:
             print("Say something!")
             audio = r.listen(source)    
             try:
-                text = r.recognize_google(audio)
+                text = r.recognize_google(audio).lower()
                 if wake.casefold() in text.casefold():
+                    print(text)
                     print("Searching Wikipedia...")
-                    text_to_speech(engine, wiki(extract_word(text)))
+                    text_to_speech(engine, wiki(extract_word(text, name)))
                 elif stop.casefold() in text.casefold():
                     text_to_speech(engine, "Shutting down. Bye.")
                     break
@@ -33,11 +35,13 @@ def recognise(r, engine, wake, stop):
             except sr.RequestError as e:
                 print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-def extract_word(text):
+def extract_word(text, name):
 
-    cleaned_text = text.translate(str.maketrans('', '', string.punctuation)).lower()
-    match = re.search(r"(hey bob what's a|hey bob what's an|hey bob what are|hey bob who is|hey bob who was|hey bob tell me about|hey bob what do you know about|hey bob what is a|hey bob what is an)\s*(.*)", cleaned_text)
+    cleaned_text = text.translate(str.maketrans('', '', string.punctuation))
+
+    match = re.search(rf"(hey {name} whats a|hey {name} whats an|hey {name} what are|hey {name} who is|hey {name} who was|hey {name} tell me about|hey {name} what do you know about|hey {name} what is a|hey {name} what is an)\s+(.*)", cleaned_text)
     if match:
+
         return match.group(2).strip()
     else:
         return None
